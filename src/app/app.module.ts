08d47fa783dfd9479270eb6428c284f4597affdf
Krzysiek K <krzysztof.kulikowski@jtendo.com>
2018-08-06 11:29:23 +0200

    Add animations, material theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { CustomMaterialModule } from './../core/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
